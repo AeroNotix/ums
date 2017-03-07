5f88b22052c724ae1af7f72b21e5c103ccad0368
Aaron France <aaron.l.france@gmail.com>
2017-03-07 10:36:52 -0700

    Add test for x-ums-session-id header

--- tests/websocket_tests.py
+++ tests/websocket_tests.py
@@ -7,3 +7,9 @@ class UMSWebsocketClientTests(unittest.TestCase):
         ws = websocket.WebSocket()
         ws.connect("ws://localhost:5564/subscribe")
         self.assertTrue(ws.connected)
+
+    def test_websocket_has_session_header(self):
+        ws = websocket.WebSocket()
+        ws.connect("ws://localhost:5564/subscribe")
+        self.assertTrue(ws.connected)
+        self.assertIsNotNone(ws.headers.get('x-ums-session-id'))
